2bd3dc3785a0e259d4e5ec439ee2bc972a91ea63
Fabien <streit.fabien@gmail.com>
2022-04-02 12:44:41 +0200

    Ajout son feu sur fourneau et hurt sur chair

--- Pickable/Pickable.gd
+++ Pickable/Pickable.gd
@@ -9,6 +9,8 @@ func _process(delta):
 	
 	if _body != null and Input.is_action_just_pressed("ui_accept"):
 		print("Pickable " + item_name + ": picked")
+		$AudioStreamPlayer.play()
+		yield($AudioStreamPlayer, "finished")
 		_body.setItem(item_name)
 		queue_free()
 
